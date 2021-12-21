dcbfc94ad7695ffd1011dbfe99e130b7494e7092
Pharrell Helstone <2153880@talnet.nl>
2021-12-21 11:24:27 +0100

    opdracht 3 af

--- 02-Select/Opdracht03-SELECT-WHERE/antwoorden.sql
+++ 02-Select/Opdracht03-SELECT-WHERE/antwoorden.sql
@@ -1,19 +1,20 @@
 -- Opdracht 1 
-
+SELECT country FROM 'jaar2015' Where rank=7
 -- Opdracht 2 
-
+SELECT * country FROM 'jaar2016' Where country= "Netherlands"
 -- Opdracht 3
-
+SELECT score FROM 'jaar2015' Where country= "Netherlands"
 -- Opdracht 4
-
+SELECT score FROM 'jaar2016' Where country= "Netherlands"
 -- Opdracht 5
-
+SELECT country , region FROM 'jaar2015' Where region= "North America"
 -- Opdracht 6 
-
+SELECT country , rank FROM 'jaar2015' Where rank= 25
 -- Opdracht 7 
-
+SELECT country , score FROM 'jaar2016' Where score > 7087
 -- Opdracht 8
-
+SELECT country region, rank FROM 'jaar2016' Where rank <= 5 "Netherlands"
 -- Opdracht 9
-
+SELECT score FROM 'jaar2016' Where counrty= "Ireland"
 -- Opdracht 10
+SELECT country , region FROM `jaar2015` WHERE region="Latin America and Caribbean"
